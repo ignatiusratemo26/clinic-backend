8d3dd2c5c9bd6e4c7ba32d9c3ef53aaa5eb57333
Ignatius Ratemo <ignatiusratemo5@gmail.com>
2024-10-18 12:09:00 +0300

    wsgi edit to connect to vercel app

--- mydocBackend/wsgi.py
+++ mydocBackend/wsgi.py
@@ -6,3 +6,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mydocBackend.settings')
 
 application = get_wsgi_application()
+# to connect it to the vercel application
+app = application
